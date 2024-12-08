52de1f9c0f23fe54938bd9b18eb7979d960c0db0
Arvind Kumar <148184868+GitHubArvind01@users.noreply.github.com>
2024-12-09 02:02:56 +0530

    Delete index_loader.js

--- index_loader.js
+++
@@ -1,7 +0,0 @@
- // Set the loader displayed flag in session storage
- sessionStorage.setItem('loaderDisplayed', 'true');
-
- // Redirect to the main page after a delay
- setTimeout(function() {
-     window.location.href = 'Main.html';
- }, 5000); // 5 seconds delay
